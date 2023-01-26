41613fef7e939aa5303dae347c81624b06ce7103
Sofia <sofia@MacBook-Air-de-Sofia.local>
2023-01-25 20:18:12 -0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import logo from "./logo.svg"
+import "./App.scss"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h1>
           Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,7 +20,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/foundation/FontSize/readme.md
@@ -0,0 +1,5 @@
+$font-name : {
+size,
+lineheight,
+weight
+}
